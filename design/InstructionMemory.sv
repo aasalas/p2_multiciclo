f560a2029b0b275a5c30adc09f4a2ed9ec4348b0
Aaron Salas <aasalas@estudiantec.cr>
2025-12-10 14:52:15 -0600

    Sets de instrucciones para la defensa

--- design/InstructionMemory.sv
+++ design/InstructionMemory.sv
@@ -19,7 +19,32 @@ module InstructionMemoryF #(parameter WIDTH=32, parameter DEPTH=64) (
 
     // Inicialización desde archivo
     initial begin
-        $readmemh("instructions.mem", Memory); // Carga desde archivo hexadecimal
+
+//$readmemh("hazard_raw1.mem", Memory); 
+// Forwarding desde etapa EX (resultado de ALU se envia a siguiente instrucción)
+// lw x4, 0(x0)        # x4 = Mem[0] (carga en MEM)
+// add x5, x4, x0      # x5 = x4 + 0 (usa x4 inmediatamente - STALL!)
+// nop
+
+//$readmemh("hazard_raw2.mem", Memory); 
+// Forwarding desde etapa MEM (datos de memoria se reutilizan)
+// sw x6, 0(x0)        # Mem[0] = x6
+// lw x7, 0(x0)        # x7 = Mem[0] (lee lo que acaba de escribir)
+// add x8, x7, x0      # x8 = x7 + 0
+
+//$readmemh("hazard_load_use.mem", Memory);
+// Load-Use hazard requiere 1 ciclo de stall porque el dato no está listo en EX
+// lw x4, 0(x0)        # x4 = Mem[0] (carga en MEM)
+// add x5, x4, x0      # x5 = x4 + 0 (usa x4 inmediatamente - STALL!)
+// nop                 # (instrucción de relleno)
+
+// $readmemh("hazard_control.mem", Memory);
+//  Control hazard con salto. Las instrucciones en IF/ID se descartan (flush)
+// addi x9, x0, 5      # x9 = 5
+// beq x9, x9, salto       # if (x9 == x9) PC += 4 (BRANCH TOMADO - FLUSH!)
+// addi x10, x0, 1     # (se descarta)
+// salto:
+// addi x11, x0, 100   # (destino del salto)
     end
 
 endmodule
